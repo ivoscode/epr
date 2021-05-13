679f0e2f0f56c0b6819ba456463b60a6bb0ff4ad
Ivo Legzdins <ivo@Ivos-MacBook-Air.local>
2021-05-13 12:37:13 +0100

    routing layout component

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,21 @@
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
+import { Context } from "../context/index";
+import Nav from "./UI/Nav/Nav";
+
 export default function Layout(props) {
+  const { state, dispatch } = useContext(Context);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.username == null) {
+      router.push("/");
+    }
+  }, [state.username]);
+
   return (
     <div className="flex flex-col w-full ">
+      <Nav />
       <main className="  mx-auto max-w-screen-2xl w-full  ">
         {props.children}
       </main>
--- pages/user/client-search.js
+++ pages/user/client-search.js
@@ -1,24 +1,14 @@
-import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
 import ClientSearchForm from "../../components/ClientSearchForm";
-import Nav from "../../components/UI/Nav/Nav";
-import { Context } from "../../context/index";
+import Layout from "../../components/layout";
 
 export default function ClientSearch() {
-  const { state, dispatch } = useContext(Context);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (state.username == null) {
-      router.push("/");
-    }
-  }, [state.username]);
   return (
-    <div>
-      <Nav />
-      <main className="bg-gray-100 p-10">
-        <ClientSearchForm />
-      </main>
-    </div>
+    <Layout>
+      <div>
+        <main className="bg-gray-100 p-10">
+          <ClientSearchForm />
+        </main>
+      </div>
+    </Layout>
   );
 }
